d78f11cf871ca2a6fbceb86401861c4a0565b0da
MonoHenry <andrepericlesnelli@gmail.com>
2024-12-14 17:04:14 -0300

    Create query for authentication

---
+++ lib/screens/login_page/login_query.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+late String token;
+
+Future<http.Response> loginUserEmail(String email, String senha) async {
+  final response = await http.post(
+    Uri.parse("http://localhost:3000/api//user/autenticaremail"),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonEncode({
+      "email": email,
+      "senha": senha,
+    }),
+  );
+  if (response.statusCode == 200) {
+    final json = jsonDecode(response.body);
+    token = json['token'];
+    return response;
+  } else {
+    return response;
+  }
+}
+
+Future<http.Response> loginUserCpf(String cpf, String senha) async {
+  final response = await http.post(
+    Uri.parse("http://localhost:3000/api//user/autenticaremail"),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonEncode({
+      "cpf": cpf,
+      "senha": senha,
+    }),
+  );
+  if (response.statusCode == 200) {
+    final json = jsonDecode(response.body);
+    token = json['token'];
+    return response;
+  } else {
+    return response;
+  }
+}
