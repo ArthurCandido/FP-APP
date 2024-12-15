5aeae4e6269ee820861a3f1d8b9c50e8de07910d
Luiz Guilherme <luguigeron@gamil.com>
2024-12-15 15:56:21 -0300

    alguma coisa

--- lib/querys/login_query.dart
+++ lib/querys/login_query.dart
@@ -3,7 +3,6 @@ import 'package:fp_app/vars.dart';
 import 'package:http/http.dart' as http;
 
 Future<http.Response> loginUserEmail(String email, String senha) async {
-  await Future.delayed(Duration(seconds: 5));
   final response = await http.post(
     Uri.parse("http://localhost:3000/api//user/autenticaremail"),
     headers: {
