106f0731c6ed467a29700decac67f9f252bb4e90
Luiz Guilherme <luguigeron@gamil.com>
2024-12-15 19:15:38 -0300

    login

---
+++ lib/components/PopupError.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class PopupError extends StatelessWidget {
+  final String error;
+
+  const PopupError({super.key, required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: Container(
+        padding: const EdgeInsets.all(32),
+        width: 200,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Text(
+            error,
+            style: const TextStyle(
+              color: Colors.red,
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
